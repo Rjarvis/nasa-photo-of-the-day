a566720a8e0c3975c464a3a3e4f1b64542140235
Roland J Jarvis <Rjarvis87@gmail.com>
2019-07-31 00:05:35 -0400

    ready to recieve api address and console.log

--- src/components/apiComponents/apiData.js
+++ src/components/apiComponents/apiData.js
@@ -1,2 +1,19 @@
 import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 
+export default function apiData(){
+    const [isLoading, setIsLoading] = useState(false);
+    useEffect(()=>{
+        setIsLoading(true);
+        axios
+            .get(/*webAddressGoesHere*/)
+            .then(res => {
+                console.log(res);
+                setIsLoading(false);
+            })
+            .catch(err =>{
+                console.log(err);
+                setIsLoading(false);
+            });
+    },[]);
+}
